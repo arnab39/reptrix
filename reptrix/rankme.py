001655919cb03b46b53c63f17ed0e72500baf2fd
Danielle Benesch <daniellerbenesch@gmail.com>
2024-02-21 20:40:38 -0500

    apply pre-commit

--- reptrix/rankme.py
+++ reptrix/rankme.py
@@ -1,6 +1,7 @@
 import numpy as np
 import utils
 
+
 def get_rank(eigen: np.ndarray) -> float:
     """Get effective rank of the representation covariance matrix
 
@@ -13,17 +14,17 @@ def get_rank(eigen: np.ndarray) -> float:
     l1 = np.sum(np.abs(eigen))
     eps = 1e-7
     scores = eigen / l1 + eps
-    entropy = - np.sum(scores * np.log(scores))
+    entropy = -np.sum(scores * np.log(scores))
     return np.exp(entropy)
 
-def get_rankme(activations: np.ndarray,
-              max_eigenvals: int = 2048) -> float:
+
+def get_rankme(activations: np.ndarray, max_eigenvals: int = 2048) -> float:
     """Get RankMe metric
     (https://proceedings.mlr.press/v202/garrido23a)
 
     Args:
         activations (np.ndarray): Activation tensor of shape (bsz,d1,d2...dn)
-        max_eigenvals (int, optional): Maximum #eigenvalues to compute. 
+        max_eigenvals (int, optional): Maximum #eigenvalues to compute.
                                     Defaults to 2048.
 
     Returns:
@@ -34,7 +35,8 @@ def get_rankme(activations: np.ndarray,
     except:
         activations_arr = activations
     activations_arr = activations_arr.cpu().numpy()
-    eigen = utils.get_eigenspectrum(activations_np=activations_arr,
-                              max_eigenvals=max_eigenvals)
+    eigen = utils.get_eigenspectrum(
+        activations_np=activations_arr, max_eigenvals=max_eigenvals
+    )
     rank = get_rank(eigen)
     return rank
--- reptrix/utils.py
+++ reptrix/utils.py
@@ -1,25 +1,27 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
-def get_eigenspectrum(activations_np: np.ndarray, 
-                      max_eigenvals: int = 2048) -> np.ndarray:
+
+def get_eigenspectrum(
+    activations_np: np.ndarray, max_eigenvals: int = 2048
+) -> np.ndarray:
     """Get eigenspectrum of activation covariance matrix.
 
     Args:
         activations_np (np.ndarray): Numpy arr of activations,
                                     shape (bsz,d1,d2...dn)
-        max_eigenvals (int, optional): Maximum #eigenvalues to compute. 
+        max_eigenvals (int, optional): Maximum #eigenvalues to compute.
                                         Defaults to 2048.
 
     Returns:
         np.ndarray: Returns the eigenspectrum of the activation covariance matrix
     """
-    feats = activations_np.reshape(activations_np.shape[0],-1)
+    feats = activations_np.reshape(activations_np.shape[0], -1)
     feats_center = feats - feats.mean(axis=0)
-    pca = PCA(n_components=min(max_eigenvals, 
-                               feats_center.shape[0], 
-                               feats_center.shape[1]), 
-                svd_solver='full')
+    pca = PCA(
+        n_components=min(max_eigenvals, feats_center.shape[0], feats_center.shape[1]),
+        svd_solver='full',
+    )
     pca.fit(feats_center)
     eigenspectrum = pca.explained_variance_ratio_
     return eigenspectrum
