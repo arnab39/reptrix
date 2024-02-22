8113117749cd27ff6c41f39b7681807439c28bcb
Arna Ghosh <arnatubaikgp@gmail.com>
2024-02-21 20:34:14 -0500

    Adding scikit-learn to dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',  # Package version
     author='Arnab Mondal, Arna Ghosh',  # Replace with your name
     author_email='arnab.mondal@mila.quebec, ghosharn@mila.quebec',  # Replace with your email
-    description='Library that provides metrics to asses representation quality',  # Package summary
+    description='Library that provides metrics to assess representation quality',  # Package summary
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/arnab39/reptrix',  # Replace with your repository URL
