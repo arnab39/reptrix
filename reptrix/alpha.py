ff6df2d3e48d6efc1af59cc86e684771330fea1a
Arna Ghosh <arnatubaikgp@gmail.com>
2024-02-21 19:45:59 -0500

    Adding utils file containing get_eigenspectrum

--- reptrix/alpha.py
+++ reptrix/alpha.py
@@ -1,6 +1,4 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from tqdm import tqdm
 from sklearn.decomposition import PCA
 from sklearn.metrics import r2_score
 
@@ -46,29 +44,6 @@ def get_powerlaw(eigen: np.ndarray, trange: np.ndarray) -> tuple:
     except:
         fit_R2_100 = None
     return (alpha, ypred, fit_R2, fit_R2_100)
-
-def get_eigenspectrum(activations_np: np.ndarray, 
-                      max_eigenvals: int = 2048) -> np.ndarray:
-    """Get eigenspectrum of activation covariance matrix.
-
-    Args:
-        activations_np (np.ndarray): Numpy arr of activations,
-                                    shape (bsz,d1,d2...dn)
-        max_eigenvals (int, optional): Maximum #eigenvalues to compute. 
-                                        Defaults to 2048.
-
-    Returns:
-        np.ndarray: Returns the eigenspectrum of the activation covariance matrix
-    """
-    feats = activations_np.reshape(activations_np.shape[0],-1)
-    feats_center = feats - feats.mean(axis=0)
-    pca = PCA(n_components=min(max_eigenvals, 
-                               feats_center.shape[0], 
-                               feats_center.shape[1]), 
-                svd_solver='full')
-    pca.fit(feats_center)
-    eigenspectrum = pca.explained_variance_ratio_
-    return eigenspectrum
 
 def get_alpha(activations: np.ndarray,
               max_eigenvals: int = 2048,
